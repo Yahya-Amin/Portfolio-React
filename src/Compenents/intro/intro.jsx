c225606790e7837f0f748a1d37f138a403348dba
amine <amin.yahya87@gmail.com>
2021-04-28 03:42:57 +0200

    component intro{] fini

--- src/Compenents/intro/intro.jsx
+++ src/Compenents/intro/intro.jsx
@@ -1,23 +1,35 @@
-import './intro.scss'
+import './intro.scss';
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+
+    const textRef = useRef();
+    useEffect(()=>{
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay:  1500,
+            backSpeed:  50,
+            strings: ['développeur web', 'désigner','integrateur web' ] 
+        })
+    },[])
     return (
         <div className="intro" id="intro">
             <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/man.png" alt=""/>
-                </div>
+               <div className="imgContainer">
+                   <img src="assets/man.png" alt=""/>
+               </div>
             </div>
             <div className="right">
-                <div className="wrapper">
-                    <h2>Salut, je suis</h2>
-                    <h1>AMINE YAHYA</h1>
-                    <h3>Freelance<span></span></h3>
-                </div>   
-            </div>
-            <a href="#Portfolio">
-                <img src="assets/down.png" alt=""/>
-            </a>
+               <div className="wrapper">
+                   <h2>Hello, Moi C'est</h2>
+                   <h1>AMINE YAHYA</h1>
+                   <h3>Freelance <span ref={textRef}></span></h3>
+               </div>
+               <a href="#portfolio">
+                   <img src="assets/down.png" alt=""/>
+               </a>
+            </div>     
         </div>
     )
 }
